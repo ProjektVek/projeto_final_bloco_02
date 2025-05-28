647f06a1303c34760610b26be70c69fa27fcc9fe
Victor Hugo <projektvek@gmail.com>
2025-05-28 11:08:39 -0300

    :sparkles: CRUD categoria finalizado

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,61 @@
+package com.generation.farmacia.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria") 
+public class Categoria {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "O nome da categoria é obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo descricao deve ter no minimo 3 e no máximo 100 caracteres")
+	private String nome;
+	
+	/*
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+	
+	
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+	*/
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
